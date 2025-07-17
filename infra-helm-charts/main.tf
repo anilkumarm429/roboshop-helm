d85ad5f30b8fc165c598f2802047d362a216cb6a
Anil Kumar M <anil.chanti429@gmail.com>
2025-07-17 23:55:46 +0530

    Roboshop Project

--- infra-helm-charts/main.tf
+++ infra-helm-charts/main.tf
@@ -68,3 +68,21 @@ KUBE
 TF
   }
 }
+
+resource "helm_release" "argocd" {
+  depends_on = [
+    null_resource.kubeconfig
+  ]
+
+  name             = "argo-cd"
+  repository       = "https://argoproj.github.io/argo-helm"
+  chart            = "argo-cd"
+  namespace        = "argocd"
+  create_namespace = true
+  set = [
+    {
+      name  = "server.service.type"
+      value = "LoadBalancer"
+    }
+  ]
+}
